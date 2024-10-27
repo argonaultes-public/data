be9754f9cc4a009077e7aea984fd7b4d4ff3382a
argonaultes <gael@argonaultes.fr>
2024-10-27 23:04:12 +0100

    init data

---
+++ data-customers.sql
@@ -0,0 +1,23 @@
+insert into my_users (email, customer, postal_adress) VALUES
+('jolie@argonaultes.fr', ROW('jolie', 'lastname', '1994-9-9'), ROW(51, 'boulevard', 89864, 'Marseille')),
+('ariel@argonaultes.fr', ROW('ariel', 'lastname', '1986-2-3'), ROW(121, 'alley', 16466, 'Versailles')),
+('dominique@argonaultes.fr', ROW('dominique', 'lastname', '2001-7-26'), ROW(4, 'avenue', 92401, 'Brest')),
+('cynthia@argonaultes.fr', ROW('cynthia', 'lastname', '2003-6-24'), ROW(109, 'boulevard', 32110, 'Lyon')),
+('yaritza@argonaultes.fr', ROW('yaritza', 'lastname', '2004-11-23'), ROW(66, 'boulevard', 87150, 'Lyon')),
+('jaidyn@argonaultes.fr', ROW('jaidyn', 'lastname', '1991-8-9'), ROW(92, 'boulevard', 8896, 'Paris')),
+('izabella@argonaultes.fr', ROW('izabella', 'lastname', '1997-4-23'), ROW(112, 'alley', 86976, 'Lyon')),
+('isabella@argonaultes.fr', ROW('isabella', 'lastname', '1989-2-5'), ROW(21, 'boulevard', 38039, 'Brest')),
+('lilliana@argonaultes.fr', ROW('lilliana', 'lastname', '1981-10-16'), ROW(43, 'avenue', 1883, 'Paris')),
+('ingrid@argonaultes.fr', ROW('ingrid', 'lastname', '1984-4-11'), ROW(78, 'alley', 72748, 'Versailles')),
+('alice@argonaultes.fr', ROW('alice', 'lastname', '1986-6-8'), ROW(17, 'avenue', 10134, 'Versailles')),
+('sasha@argonaultes.fr', ROW('sasha', 'lastname', '1989-4-12'), ROW(83, 'avenue', 72425, 'Bordeaux')),
+('michelle@argonaultes.fr', ROW('michelle', 'lastname', '1993-7-22'), ROW(55, 'avenue', 48120, 'Paris')),
+('joy@argonaultes.fr', ROW('joy', 'lastname', '1993-3-27'), ROW(123, 'boulevard', 58148, 'Bordeaux')),
+('nathaly@argonaultes.fr', ROW('nathaly', 'lastname', '1991-11-23'), ROW(136, 'avenue', 24746, 'Lyon')),
+('rylie@argonaultes.fr', ROW('rylie', 'lastname', '1984-8-5'), ROW(14, 'avenue', 28943, 'Paris')),
+('jaslene@argonaultes.fr', ROW('jaslene', 'lastname', '1993-3-26'), ROW(123, 'avenue', 33813, 'Lyon')),
+('taylor@argonaultes.fr', ROW('taylor', 'lastname', '1996-7-10'), ROW(56, 'alley', 17265, 'Paris')),
+('madelynn@argonaultes.fr', ROW('madelynn', 'lastname', '2003-1-5'), ROW(66, 'avenue', 82381, 'Lyon')),
+('jordan@argonaultes.fr', ROW('jordan', 'lastname', '1981-8-19'), ROW(42, 'boulevard', 60943, 'Marseille'))
+;
+
